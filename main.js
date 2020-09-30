e88efb2367c92e623f68d659684580765b3637b1
Adrien Desrames <adriendesrames.ios@gmail.com>
2020-10-01 01:23:02 +0200

    Update border setting from btn Add GDPR section and update main js for GDPR.

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ window.addEventListener('scroll', function(){
 hamburgerMenu.addEventListener('click', function() {
   header.classList.toggle('menu-open');
 })
+
+// Cookie
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true")
+});
+
+setTimeout(() => {
+  if(!localStorage.getItem("cookieBannerDisplayed"))
+    cookieContainer.classList.add("active");
+}, 2000);
