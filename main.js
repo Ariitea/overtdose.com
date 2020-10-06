7501b4fadcc1ba8c5fe6585e210c2df791bcef11
Adrien Desrames <adriendesrames.ios@gmail.com>
2020-10-06 14:31:23 +0200

    Maintenance function on Update Design Update main js for menu settings

--- main.js
+++ main.js
@@ -8,25 +8,31 @@ let itemOnClick = document.querySelector("nav-item");
 
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("cookieBannerDisplayed", "true")
-});
 
-// Hamburger Menu
+
+// Hamburger Menu animation
 window.addEventListener('scroll', function(){
   let windowPosition = window.scrollY > 0;
   header.classList.toggle('active', windowPosition)
 });
 
+// Hamburger Menu open function
 hamburgerMenu.addEventListener('click', function() {
   header.classList.toggle('menu-open')
 });
 
-//Hamburger Menu Down On Click
+//Hamburger Menu "show/hide" On Click
+itemOnClick.addEventListener('click', function(){
+  header.classList.remove('menu-open')
+});
 
+// Cookie fucntion for storage et remove banner
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true")
+});
 
-// Cookie
+// Time out storage displayed
 setTimeout(() => {
   if(!localStorage.getItem("cookieBannerDisplayed"))
     cookieContainer.classList.add("active")
