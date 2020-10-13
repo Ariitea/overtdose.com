80ae3249e04045477ab4bd0551ba6191fee7b81d
Adrien Desrames <adriendesrames.ios@gmail.com>
2020-10-13 16:49:19 +0200

    add storage variable for Store Firebase

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ firebase.initializeApp({
 });
 
 var db = firebase.firestore();
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
 
 
 
